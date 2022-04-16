933e3de9de0d0d9b430a1f4e00a02c1a73cea633
Rutwik187 <rutwikshinde34@gmail.com>
2022-04-16 20:18:50 +0530

    Complete website 1

--- Cakes/script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementById("menuToggle");
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-
-toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
-});
---
+++ Home-Page/Home.js
@@ -0,0 +1,54 @@
+// Corousal
+
+const slider = document.querySelector(".corousal");
+let isDown = false;
+let startX;
+let scrollLeft;
+slider.scrollLeft = 357.6;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+  console.log(slider.scrollLeft);
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return; // stop the fn from running
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2;
+  slider.scrollLeft = scrollLeft - walk;
+});
+
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
